0424415605885a145c6737e03af24e920538cf40
Morgan Jones <wynjmorgan@gmail.com>
2022-02-20 23:56:56 +0000

    Built!

---
+++ dist/core/handlers/EventHandler.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=EventHandler.js.map
---
+++ dist/index.js
@@ -0,0 +1,2 @@
+var n=require("dotenv/config"),o=require("./core/bot");const t=new o.Bot;t.login(process.env.TOKEN);
+//# sourceMappingURL=index.js.map
