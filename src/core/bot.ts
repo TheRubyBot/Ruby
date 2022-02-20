53d0de476087788e4e0c0dde1d1993ca59aac588
Morgan Jones <wynjmorgan@gmail.com>
2022-02-20 23:00:21 +0000

    Added cool version stuff

--- src/core/bot.ts
+++ src/core/bot.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import { Client } from "discord.js";
+import { readFileSync } from "fs";
 
 export class Bot {
   // Clients
@@ -8,8 +9,17 @@ export class Bot {
   });
   public prisma: PrismaClient = new PrismaClient();
 
+  public version: { number: string; revision: number; codename: string };
+
   constructor() {
     this.client.on("ready", () => console.log(`Logged in as ${this.client.user?.tag}`));
+
+    const pjson = JSON.parse(readFileSync("./package.json", "utf8"));
+    this.version = {
+      number: pjson.version,
+      revision: pjson.revision,
+      codename: pjson.codename
+    };
   }
 
   public login(token: string): void {
